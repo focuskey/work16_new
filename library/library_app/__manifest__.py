b7ec2916af32633581b531c4d247aaf5b76373dc
focuskey <focuskey@gmail.com>
2022-11-28 17:47:37 -0800

    take a photo

---
+++ library/library_app/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Library Management",
+    "summary": "Manage Library catelog and book lending.",
+    "author" : "William Wei",
+    "license": "AGPL-3",
+    "website": "https://carpetcall.com.au",
+    "version": "16.0.1.0.0",
+    "category": "Services/Library",
+    "depends": ["base"],
+    "application": True,
+}
